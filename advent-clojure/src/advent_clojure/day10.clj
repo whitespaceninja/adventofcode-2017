b5ef6590f902954d99937429b1ef26309fb58147
David Bachowski <dave@whitespace.ninja>
2017-12-11 10:22:13 -0500

    Refactoring day10 to be more concise

--- advent-clojure/src/advent_clojure/day10.clj
+++ advent-clojure/src/advent_clojure/day10.clj
@@ -12,64 +12,60 @@
   (vec (concat (subvec rope at-indx)
                (take at-indx rope))))
   
-
 (defn twist
-  [skip-size rope lengths offset]
-  (let [length (first lengths)
-        lengths (rest lengths)
-        reversed (reverse (take length rope))
-        reversed-rope (vec (concat reversed (subvec rope length)))
-        new-start (add-within rope skip-size length)
-        new-rope (cut-and-tie reversed-rope new-start)
-        new-offset (add-within rope
-                               offset
-                               (- (count rope)
-                                  (+ skip-size length)))
-        new-skip-size (+ 1 skip-size)]
-    (if (<= (count lengths) 0)
-      {:rope new-rope :offset new-offset :skip-size new-skip-size}
-      (recur new-skip-size new-rope lengths new-offset))))
-
-(defn parse-input
-  [input]
-  (->> (str/split input #",")
-       (map read-string)
-       vec))
+  ([rope lengths]
+   (twist rope lengths 0 0))
+  ([rope lengths skip-size offset]
+   (let [length (first lengths)
+         lengths (rest lengths)
+         new-start (add-within rope skip-size length)
+         new-rope (->> (reverse (take length rope))
+                       (#(concat % (subvec rope length)))
+                       (vec)
+                       (#(cut-and-tie % new-start)))
+         new-offset (add-within rope
+                                offset
+                                (- (count rope)
+                                   (+ skip-size length)))
+         new-skip-size (+ 1 skip-size)]
+     (if (<= (count lengths) 0)
+       {:rope new-rope :offset new-offset :skip-size new-skip-size}
+       (recur new-rope lengths new-skip-size new-offset)))))
 
 (defn twist-n-times
-  [rope skip-size offset lengths n]
-  (let [answer (twist skip-size rope lengths offset)
-        _ (println (str "n " n " answer: " answer))]
-    (if (= n 1)
-      answer
-      (recur (:rope answer)
-             (:skip-size answer)
-             (:offset answer)
-             lengths
-             (- n 1)))))
+  ([rope lengths n]
+   (twist-n-times rope lengths n 0 0))
+  ([rope lengths n skip-size offset]
+   (let [answer (twist rope lengths skip-size offset)]
+     (if (= n 1)
+       answer
+       (recur (:rope answer)
+              lengths
+              (- n 1)
+              (:skip-size answer)
+              (:offset answer))))))
 
-
+;; hardcoded ending to the lengths as described by puzzle
 (def HARD_END [17 31 73 47 23])
 
 (defn puzzle-10-2
   [rope input]
-  (let [lengths (concat (map int (char-array input)) HARD_END)
-        after-twists (twist-n-times rope 0 0 lengths 64)
-        rope-reset (cut-and-tie (:rope after-twists) (:offset after-twists))
-        partitions (partition 16 rope-reset)
-        partitions-xor (map #(apply bit-xor %) partitions)
-        hexed (map #(format "%x" %) partitions-xor)]
-    (reduce str hexed)))
+  (let [lengths (concat (map int (char-array input)) HARD_END)]
+    (->> (twist-n-times rope lengths 64)
+         (#(cut-and-tie (:rope %) (:offset %)))
+         (partition 16)
+         (map #(apply bit-xor %))
+         (map #(format "%x" %))
+         (reduce str))))
 
 (defn puzzle-10-1
   [rope input]
-  (let [lengths (parse-input input)
-        answer (twist 0 rope lengths 0)
-        final-rope (:rope answer)
-        offset (:offset answer)
-        first-char (nth final-rope offset)
-        second-char (nth final-rope (add-within final-rope 1 offset))
-        mult (* first-char second-char)]
+  (let [lengths (->> (str/split input #",")
+                     (map read-string)
+                     vec)
+        answer (twist rope lengths)
+        final-rope (cut-and-tie (:rope answer) (:offset answer))
+        mult (* (first final-rope) (second final-rope))]
     [mult answer]))
     
      
